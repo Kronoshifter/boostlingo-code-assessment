f98f623221523d55520092e113aeaf4e27d47092
Kronos <Kronoshifter@gmail.com>
2024-08-07 14:13:32 -0500

    added header/toolbar for home page

--- src/component/home/home.component.ts
+++ src/component/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { MatToolbar } from '@angular/material/toolbar'
+import { SessionService } from '../../services/session.service'
+import { MatButton } from '@angular/material/button'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatButton,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  email: string | null = ''
+
+  constructor(private session: SessionService) {
+
+  }
+
+  ngOnInit(): void {
+    this.email = this.session.session
+  }
+
+  logout() {
+    this.session.logout()
+  }
 
 }
