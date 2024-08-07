02477d10ea33197941ab93e266df3d1ae4689c8d
Kronos <Kronoshifter@gmail.com>
2024-08-07 15:10:00 -0500

    more input validation for login page

--- src/component/login/login.component.ts
+++ src/component/login/login.component.ts
@@ -4,7 +4,7 @@ import { MatFormFieldModule} from '@angular/material/form-field'
 import { MatInput } from '@angular/material/input'
 import { MatButton } from '@angular/material/button'
 import { SessionService } from '../../services/session.service'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, NgForm } from '@angular/forms'
 import { Router } from '@angular/router'
 import { MatSnackBar } from '@angular/material/snack-bar'
 
@@ -40,12 +40,12 @@ export class LoginComponent implements OnInit {
     this.session.login()
   }
 
-  authenticateAndLogin() {
-    if (!!this.email.trim() && !!this.password.trim()) {
+  authenticateAndLogin(form: NgForm) {
+    if (this.validateInputs(form)) {
       this.session.authenticateUser(this.email)
       this.login()
     } else {
-      this.showSnackbar('Please enter email and password')
+      this.showSnackbar('Please enter valid email and password')
     }
   }
 
@@ -53,5 +53,9 @@ export class LoginComponent implements OnInit {
     this.snackbar.open(message, 'dismiss', {
       duration: 3000
     })
+  }
+
+  validateInputs(form: NgForm): boolean {
+    return (form.valid && !form.pristine && !!this.email.trim() && !!this.password.trim()) as boolean
   }
 }
